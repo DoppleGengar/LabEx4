eee2ca8da7a97408185a8bc0e824ad6f03eb2fdd
Nebye <56895013+Nebye@users.noreply.github.com>
2020-02-04 19:13:17 -0700

    Resolving Final Conflicts

--- CMPUT301_Lab04/app/src/main/java/com/example/cmput301_lab04/Circle.java
+++ CMPUT301_Lab04/app/src/main/java/com/example/cmput301_lab04/Circle.java
@@ -2,8 +2,8 @@ package com.example.cmput301_lab04;
 
 public class Circle extends Shape
 {
-    public Circle(int x, int y)
+    public Circle(int x, int y, String color)
     {
-        super(x,y);
+        super(x,y, "magenta");
     }
 }
